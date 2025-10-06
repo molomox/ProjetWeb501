7d1c9deee99b7d2ea9da34319bf1f9c0a0015ea2
habicll <habi.cailleau@algosup.com>
2025-10-06 15:48:40 +0200

    frameworks

---
+++ LinkUpBack/src/main/java/com/projet_web/LinkUpBack/LinkUpBackApplication.java
@@ -0,0 +1,13 @@
+package com.projet_web.LinkUpBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkUpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkUpBackApplication.class, args);
+	}
+
+}
---
+++ LinkUpBack/src/test/java/com/projet_web/LinkUpBack/LinkUpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_web.LinkUpBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkUpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
